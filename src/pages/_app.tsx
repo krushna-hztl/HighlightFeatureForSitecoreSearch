f596303727a61d715b3f5c96002d7bd3e3412ac7
Krushna <krushna.patel@horizontal.com>
2024-08-09 14:34:04 +0530

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Environment, WidgetsProvider } from "@sitecore-search/react";
+import { PageController } from "@sitecore-search/react";
+import "../styles/styles.css";
 
+const context = PageController.getContext();
+
+context.setLocaleLanguage("en");
+context.setLocaleCountry("us");
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WidgetsProvider
+      env={process.env.NEXT_PUBLIC_ENV as Environment}
+      customerKey={process.env.NEXT_PUBLIC_CUSTOMER_ID}
+      apiKey={process.env.NEXT_PUBLIC_API_KEY}
+    >
+      <Component {...pageProps} />;
+    </WidgetsProvider>
+  );
 }
